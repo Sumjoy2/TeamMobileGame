d8bb6f6548086a3e44259ce3f854e90bf9805aca
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-07 13:10:57 -0800

    StarterStuff

---
+++ Assets/Scipting/MenuButtons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
