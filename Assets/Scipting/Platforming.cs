5b0f29ef18f3dfd182df457853a4b1cca22616a0
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-15 13:29:26 -0800

    PlatformsAndPlayerMovement

--- Assets/Scipting/Platforming.cs
+++ Assets/Scipting/Platforming.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class Platforming : MonoBehaviour
 {
-    // a is called before the first frame update
-    private void awake()
-    {
+    private Vector2 moving;
 
+    // a is called before the first frame update
+    private void Awake()
+    {
+        moving = new Vector2(0,-0.05f);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 tmp = transform.position;
+        tmp += moving; 
+        transform.position = tmp;
+
+        if (tmp.y <= -12)
+        {
+            transform.position = new Vector2(Random.Range(-3.5f, 3.5f), 10);
+        }
     }
 }
--- Assets/Scipting/Player.cs
+++ Assets/Scipting/Player.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public float speed = 3.5f;
-    Rigidbody2D rigidBody;
-    int Score = 0;
+    Rigidbody2D rigidbody2d;
+    public int Score = 0;
     public GameObject winText;
 
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         Application.targetFrameRate = 60;
     }
 
@@ -22,10 +22,30 @@ public class Player : MonoBehaviour
         {
             LoadScene("MainMenu");
         }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x < 0)
+            {
+                transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
+            }
+            if (mousePos.x > 0)
+            {
+                transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Score++;
     }
 
     private void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
 }
