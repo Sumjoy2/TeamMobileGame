353202685eabf42d0e5c209139f66c4cc3c0acf1
Sumjoy2 <5011286mountjoy@gmail.com>
2023-02-28 13:45:07 -0800

    raycastTimer

--- Assets/Scipting/Player.cs
+++ Assets/Scipting/Player.cs
@@ -9,7 +9,9 @@ public class Player : MonoBehaviour
     public float speed = 3.5f;
     public int Score = 0;
     public float uppies = 0.1f;
-    public TMP_Text winText;
+    public float invTimer = 0.0f;
+    private float timerCollider;
+    public GameObject winText;
     public TMP_Text scoreText;
 
     Rigidbody2D rigidbody2d;
@@ -21,32 +23,41 @@ public class Player : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         Application.targetFrameRate = 60;
 
-        // textMeshPro = gameObject.GetComponent<TextMeshProUGUI>();
-
+        //textMeshPro = gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
     {
-        RaycastHit2D up = Physics2D.Raycast(rigidbody2d.position * uppies, Vector2.up);
-        RaycastHit2D down = Physics2D.Raycast(rigidbody2d.position * -uppies, Vector2.down);
+        /*********************
+         * New plan
+         * timer that after up raycast hits something wait .5 to 1 sec then re enable players Circle Collider
+         * ******************/
+        
 
-        Vector2 upDebug = transform.TransformDirection(Vector2.up) * uppies;
-        Vector2 downDebug = transform.TransformDirection(Vector2.down) * uppies;
+        RaycastHit2D up = Physics2D.Raycast(rigidbody2d.position * -uppies, Vector2.down);
+
+        Vector2 upDebug = transform.TransformDirection(Vector2.down) * uppies;
 
         Debug.DrawRay(rigidbody2d.position, upDebug, Color.red);
-        Debug.DrawRay(rigidbody2d.position, downDebug, Color.green);
 
         if (up.collider != null)
         {
             //disable collider
-            GetComponent<CircleCollider2D>().enabled = false;
-            Debug.Log("Help");
+                GetComponent<CircleCollider2D>().enabled = true;
+                Debug.Log("Help");
+                timerCollider = invTimer;
         }
-        if (down.collider != null)
+
+        if (timerCollider > 0)
+        {
+            timerCollider -= Time.deltaTime;
+        }
+        else
         {
             //enable collider
-            GetComponent<CircleCollider2D>().enabled = true;
+            GetComponent<CircleCollider2D>().enabled = false;
             Debug.Log("Uppies");
+            timerCollider = 0.0f;
         }
     }
 
@@ -76,8 +87,9 @@ public class Player : MonoBehaviour
 
         if (Score >= 15)
         {
-            //winText.enable = true;
-            LoadScene("MainMenu");
+            winText.SetActive(true);
+            //Wait like 3 seconds
+            //LoadScene("MainMenu");
         }
     }
 
